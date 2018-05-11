bdbb2302ca521ef4f5506de684c538229fde4243
stas <sheffmail@mail.ru>
2018-05-11 17:33:16 +0300

    full vdpau2tf video rendering + measuring performance

--- vdapp/main.cpp
+++ vdapp/main.cpp
@@ -19,6 +19,12 @@
 
 #include <boost/shared_ptr.hpp>
 
+extern "C" {
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libavcodec/vdpau.h>
+}
+
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 #include <X11/XKBlib.h>
@@ -30,11 +36,16 @@
 #include <X11/extensions/xf86vmode.h>
 #include <vdpau/vdpau_x11.h>
 #include <malloc.h>
+#include <unistd.h>
+#include <sys/time.h>
 
 #include <cuda.h>
 #include <cudaVDPAU.h>
 
 #undef Status
+
+#define OUT_WIDTH 128
+#define OUT_HEIGHT 72
 
 class VDPAUAllocator : public tensorflow::Allocator
 {
@@ -58,15 +69,18 @@ private:
 	void* vdpau_ptr;
 };
 
-#define MY_VID_WIDTH 128
-#define MY_VID_HEIGHT 72
+struct VideoBuffer
+{
+	VdpVideoSurface video_surface;
+	bool busy = false;
+};
 
 static Display* dpy = NULL;
 static Window win = 0;
 static GC gc = 0;
 
 static VdpOutputSurface output_surface;
-static VdpVideoSurface video_surface;
+static VideoBuffer video_buffers[3];
 static VdpProcamp procamp;
 static VdpVideoMixer video_mixer;
 static uint32_t vid_width, vid_height;
@@ -75,6 +89,7 @@ static VdpDevice vdp_device;
 static int colorspace;
 static VdpPresentationQueueTarget vdp_target;
 static VdpPresentationQueue vdp_queue;
+static VdpDecoder vdp_decoder;
 
 static VdpGetProcAddress *vdp_get_proc_address;
 static VdpDeviceDestroy *vdp_device_destroy;
@@ -97,7 +112,212 @@ static VdpPresentationQueueTargetCreateX11 *vdp_presentation_queue_target_create
 static VdpDecoderCreate *vdp_decoder_create;
 static VdpDecoderDestroy *vdp_decoder_destroy;
 static VdpDecoderRender *vdp_decoder_render;
+static VdpDecoderQueryCapabilities *vdp_decoder_query_capabilities;
+static VdpDecoderGetParameters *vdp_decoder_get_parameters;
 static VdpGenerateCSCMatrix *vdp_generate_csc_matrix;
+
+static enum AVPixelFormat get_format_vdpau(struct AVCodecContext *avctx,
+										   const enum AVPixelFormat *valid_fmts);
+static int get_buffer_vdpau(struct AVCodecContext *c, AVFrame *pic, int flags);
+
+class CompressedVideoStream
+{
+public:
+	explicit CompressedVideoStream(const std::string& filename)
+	{
+		av_register_all();
+		avcodec_register_all();
+		avformat_network_init();
+
+		int r;
+		AVDictionary* dict = 0;
+		r = avformat_open_input(&format_, filename.c_str(), 0, &dict);
+		assert(format_);
+
+		r = avformat_find_stream_info(format_, NULL);
+		assert(r >= 0);
+
+		for (int c = 0; c < (int)format_->nb_streams; c++) {
+			AVCodecContext* cx = format_->streams[c]->codec;
+			if (cx->codec_type==AVMEDIA_TYPE_VIDEO) {
+				assert(cx->codec_id != AV_CODEC_ID_NONE && cx->codec_id != AV_CODEC_ID_RAWVIDEO);
+				cx_video_ = format_->streams[c]->codec;
+				video_stream_index_ = c;
+				break;
+			}
+		}
+
+		assert(video_stream_index_ != -1);
+
+		AVCodec* tmp_dec = avcodec_find_decoder(format_->streams[video_stream_index_]->codecpar->codec_id);
+		assert(tmp_dec);
+
+		cx_video_hw_ = avcodec_alloc_context3(tmp_dec);
+		r = avcodec_parameters_to_context(cx_video_hw_, format_->streams[video_stream_index_]->codecpar);
+		assert(r >= 0);
+		av_codec_set_pkt_timebase(cx_video_hw_, format_->streams[video_stream_index_]->time_base);
+
+		cx_video_hw_->opaque = (void*)this;
+		cx_video_hw_->get_buffer2 = get_buffer_vdpau;
+		cx_video_hw_->get_format = get_format_vdpau;
+		cx_video_hw_->slice_flags = SLICE_FLAG_CODED_ORDER | SLICE_FLAG_ALLOW_FIELD;
+
+		cx_video_hw_->err_recognition = AV_EF_COMPLIANT;
+		cx_video_hw_->workaround_bugs = FF_BUG_AUTODETECT;
+		cx_video_hw_->error_concealment = FF_EC_GUESS_MVS | FF_EC_DEBLOCK;
+		cx_video_hw_->idct_algo = FF_IDCT_AUTO;
+		cx_video_hw_->debug = 0;
+
+		vdpau_context_ = av_vdpau_alloc_context();
+		memset(vdpau_context_, 0, sizeof(AVVDPAUContext));
+
+		width_ = cx_video_->width;
+		height_ = cx_video_->height;
+
+		r = avcodec_open2(cx_video_hw_, tmp_dec, NULL);
+		assert(r >= 0);
+	}
+
+	~CompressedVideoStream()
+	{
+		avformat_close_input(&format_);
+	}
+
+	bool get_next_frame()
+	{
+		AVPacket pkt;
+
+		int ret = av_read_frame(format_, &pkt);
+		if (ret < 0) {
+			return false;
+		}
+
+		if (pkt.stream_index != video_stream_index_) {
+			return true;
+		}
+
+		while (true) {
+			bool resend_packet = false;
+			int ret = avcodec_send_packet(cx_video_hw_, &pkt);
+			if (ret < 0 && ret == AVERROR(EAGAIN))
+				resend_packet = true;
+			else if (ret < 0)
+				break;
+
+			AVFrame* frame = av_frame_alloc();
+
+			ret = avcodec_receive_frame(cx_video_hw_, frame);
+
+			if (ret == AVERROR(EAGAIN) || resend_packet) {
+				av_frame_unref(frame);
+				continue;
+			} else if (ret == 0) {
+				av_frame_unref(frame);
+				break;
+			} else if (ret < 0) {
+				printf("DAMN!\n");
+				av_frame_unref(frame);
+				break;
+			}
+		}
+
+		av_packet_unref(&pkt);
+
+		return true;
+	}
+
+	int get_width() const { return width_; }
+	int get_height() const { return height_; }
+
+	AVFormatContext* format_ = 0;
+	AVCodecContext* cx_video_ = 0;
+	int video_stream_index_ = -1;
+	AVCodecContext* cx_video_hw_;
+	AVVDPAUContext* vdpau_context_;
+	AVFrame* frame_;
+
+	int width_ = 0;
+	int height_ = 0;
+};
+
+static int render_vdpau(struct AVCodecContext *s, AVFrame *src,
+	const VdpPictureInfo *info,
+	uint32_t count,
+	const VdpBitstreamBuffer *buffers)
+{
+	if (!src)
+		return -1;
+
+	VideoBuffer* buff = (VideoBuffer*)src->opaque;
+
+	VdpStatus vdp_st = vdp_decoder_render(vdp_decoder, buff->video_surface, info, count, buffers);
+	assert(vdp_st == VDP_STATUS_OK);
+
+	VdpVideoMixerPictureStructure field = VDP_VIDEO_MIXER_PICTURE_STRUCTURE_FRAME;
+
+	vdp_video_mixer_render(video_mixer, VDP_INVALID_HANDLE, 0,
+		field, 0, (VdpVideoSurface*)VDP_INVALID_HANDLE,
+		buff->video_surface,
+		0, (VdpVideoSurface*)VDP_INVALID_HANDLE,
+		NULL,
+		output_surface,
+		NULL, NULL, 0, NULL);
+
+	return 0;
+}
+
+static enum AVPixelFormat get_format_vdpau(struct AVCodecContext *avctx,
+										   const enum AVPixelFormat *valid_fmts)
+{
+	CompressedVideoStream* this_ = (CompressedVideoStream*)avctx->opaque;
+	avctx->hwaccel_context = this_->vdpau_context_;
+	int r = av_vdpau_bind_context(avctx, vdp_device, vdp_get_proc_address, 0);
+	assert(r == 0);
+	((AVVDPAUContext*)(avctx->hwaccel_context))->render2 = render_vdpau;
+
+	while (*valid_fmts != AV_PIX_FMT_NONE) {
+		if (avctx->hwaccel_context and (*valid_fmts == AV_PIX_FMT_VDPAU))
+			return AV_PIX_FMT_VDPAU;
+		if (!avctx->hwaccel_context and (*valid_fmts == AV_PIX_FMT_YUV420P))
+			return AV_PIX_FMT_YUV420P;
+		valid_fmts++;
+	}
+	return AV_PIX_FMT_NONE;
+}
+
+static void release_buffer_vdpau(void *opaque, uint8_t *data)
+{
+	VideoBuffer* buff = (VideoBuffer*)data;
+	buff->busy = false;
+}
+
+static int get_buffer_vdpau(struct AVCodecContext *c, AVFrame *pic, int /*flags*/)
+{
+	int buff_i = -1;
+	for (size_t i = 0; i < sizeof(video_buffers)/sizeof(video_buffers[0]); ++i) {
+		if (!video_buffers[i].busy) {
+			buff_i = i;
+			video_buffers[i].busy = true;
+			break;
+		}
+	}
+
+	assert(buff_i != -1);
+
+	for (int i = 0; i < 4; i++) {
+		pic->data[i] = NULL;
+		pic->linesize[i] = 0;
+	}
+	pic->opaque = &video_buffers[buff_i];
+	pic->reordered_opaque = c->reordered_opaque;
+	pic->data[3] = (uint8_t*)(uintptr_t)video_buffers[buff_i].video_surface;
+
+	AVBufferRef *buffer =
+		av_buffer_create((uint8_t*)&video_buffers[buff_i], 0, release_buffer_vdpau, NULL, 0);
+	pic->buf[0] = buffer;
+
+	return 0;
+}
 
 static void initX()
 {
@@ -107,7 +327,7 @@ static void initX()
 	black = BlackPixel(dpy, DefaultScreen(dpy)),
 	white = WhitePixel(dpy, DefaultScreen(dpy));
 	win = XCreateSimpleWindow(dpy, DefaultRootWindow(dpy), 0, 0,
-			MY_VID_WIDTH, MY_VID_HEIGHT, 5, black, white);
+			OUT_WIDTH, OUT_HEIGHT, 5, black, white);
 	XSetStandardProperties(dpy, win, "VDPAU Test", "VDPAU",
 			None, NULL, 0, NULL);
 	XSelectInput(dpy, win, ExposureMask|ButtonPressMask|KeyPressMask);
@@ -154,10 +374,12 @@ static int initVDPAU()
 		{VDP_FUNC_ID_PRESENTATION_QUEUE_DESTROY,        &vdp_presentation_queue_destroy},
 		{VDP_FUNC_ID_PRESENTATION_QUEUE_DISPLAY,        &vdp_presentation_queue_display},
 		{VDP_FUNC_ID_PRESENTATION_QUEUE_TARGET_CREATE_X11,
-						&vdp_presentation_queue_target_create_x11},
+			&vdp_presentation_queue_target_create_x11},
 		{VDP_FUNC_ID_DECODER_CREATE,                    &vdp_decoder_create},
 		{VDP_FUNC_ID_DECODER_RENDER,                    &vdp_decoder_render},
 		{VDP_FUNC_ID_DECODER_DESTROY,                   &vdp_decoder_destroy},
+		{VDP_FUNC_ID_DECODER_QUERY_CAPABILITIES,        &vdp_decoder_query_capabilities},
+		{VDP_FUNC_ID_DECODER_GET_PARAMETERS,            &vdp_decoder_get_parameters},
 		{VDP_FUNC_ID_GENERATE_CSC_MATRIX,               &vdp_generate_csc_matrix},
 		{0, NULL}
 	};
@@ -256,13 +478,11 @@ static int createVDPMixer(VdpChromaType vdp_chroma_type)
 static int createVideoSurface()
 {
 	VdpStatus vdp_st;
-
-	vdp_st = vdp_video_surface_create(vdp_device, vdp_chroma_type,
-										   vid_width, vid_height,
-										   &video_surface);
-
-	if (vdp_st != VDP_STATUS_OK)
-		return -1;
+	for (size_t i = 0; i < sizeof(video_buffers)/sizeof(video_buffers[0]); ++i) {
+		vdp_st = vdp_video_surface_create(vdp_device, vdp_chroma_type,
+			vid_width, vid_height, &video_buffers[i].video_surface);
+		assert(vdp_st == VDP_STATUS_OK);
+	}
 
 	return 0;
 }
@@ -272,8 +492,8 @@ static int createOutputSurface()
 	VdpStatus vdp_st;
 
 	vdp_st = vdp_output_surface_create(vdp_device, VDP_RGBA_FORMAT_B8G8R8A8,
-										   vid_width, vid_height,
-										   &output_surface);
+		OUT_WIDTH, OUT_HEIGHT,
+		&output_surface);
 
 	if (vdp_st != VDP_STATUS_OK)
 		return -1;
@@ -281,30 +501,10 @@ static int createOutputSurface()
 	return 0;
 }
 
-static int putBits()
+static void createDecoder()
 {
-	VdpStatus vdp_st;
-
-	void* planes[3];
-	uint32_t pitches[3] = {vid_width, vid_width / 2, vid_width / 2};
-
-	cv::Mat frame = cv::imread("car.png");
-	cv::resize(frame, frame, cv::Size(vid_width, vid_height), 0, 0, cv::INTER_AREA);
-	cv::cvtColor(frame, frame, CV_BGR2YUV_YV12);
-
-	planes[0] = frame.data;
-	planes[1] = frame.data + vid_width * vid_height;
-	planes[2] = frame.data + vid_width * vid_height + vid_width * vid_height / 4;
-
-	vdp_st = vdp_video_surface_put_bits_y_cb_cr(video_surface, VDP_YCBCR_FORMAT_YV12,
-		planes, pitches);
-
-	if (vdp_st != VDP_STATUS_OK) {
-		printf("error = %d\n", (int)vdp_st);
-		return -1;
-	}
-
-	return 0;
+	VdpStatus vdp_st = vdp_decoder_create(vdp_device, VDP_DECODER_PROFILE_H264_HIGH, vid_width, vid_height, 3, &vdp_decoder);
+	assert(vdp_st == VDP_STATUS_OK);
 }
 
 static cv::Mat getBits()
@@ -312,9 +512,9 @@ static cv::Mat getBits()
 	VdpStatus vdp_st;
 
 	void* planes[1];
-	uint32_t pitches[1] = {vid_width * 4};
+	uint32_t pitches[1] = {OUT_WIDTH * 4};
 
-	cv::Mat data(vid_height, vid_width, CV_8UC4);
+	cv::Mat data(OUT_HEIGHT, OUT_WIDTH, CV_8UC4);
 
 	planes[0] = data.data;
 
@@ -346,7 +546,7 @@ static void initVDPAUCuda(int device_id)
 	res = cuGraphicsVDPAURegisterOutputSurface(&vdpau_cuda_resource, output_surface, CU_GRAPHICS_MAP_RESOURCE_FLAGS_READ_ONLY);
 	assert(res == CUDA_SUCCESS);
 
-	res = cuMemAlloc(&vdpau_cuda_data, vid_width * vid_height * 4);
+	res = cuMemAlloc(&vdpau_cuda_data, OUT_WIDTH * OUT_HEIGHT * 4);
 	assert(res == CUDA_SUCCESS);
 }
 
@@ -414,38 +614,17 @@ static tensorflow::Status initTF(const std::string& graph_file, int device_id)
 	return tensorflow::Status::OK();
 }
 
-static void* TFVDPAUmap()
+static CUarray array = NULL;
+
+static void TFVDPAUmap()
 {
 	CUgraphicsResource resources[1] = { vdpau_cuda_resource };
 
 	CUresult res = cuGraphicsMapResources(1, resources, tf_cuda_stream);
 	assert(res == CUDA_SUCCESS);
 
-	CUarray array = NULL;
-
 	res = cuGraphicsSubResourceGetMappedArray(&array, vdpau_cuda_resource, 0, 0);
 	assert(res == CUDA_SUCCESS);
-
-	CUDA_MEMCPY2D cpyinfo;
-
-	cpyinfo.srcMemoryType = CU_MEMORYTYPE_ARRAY;
-	cpyinfo.srcHost = 0;
-	cpyinfo.srcArray = array;
-	cpyinfo.srcXInBytes = 0;
-	cpyinfo.srcY = 0;
-	cpyinfo.srcPitch = vid_width * 4;
-	cpyinfo.dstMemoryType = CU_MEMORYTYPE_DEVICE;
-	cpyinfo.dstDevice = vdpau_cuda_data;
-	cpyinfo.dstXInBytes = 0;
-	cpyinfo.dstY = 0;
-	cpyinfo.dstPitch = vid_width * 4;
-	cpyinfo.WidthInBytes = vid_width * 4;
-	cpyinfo.Height = vid_height;
-
-	res = cuMemcpy2DAsync(&cpyinfo, tf_cuda_stream);
-	assert(res == CUDA_SUCCESS);
-
-	return (void*)vdpau_cuda_data;
 }
 
 static void TFVDPAUunmap()
@@ -456,8 +635,8 @@ static void TFVDPAUunmap()
 	assert(res == CUDA_SUCCESS);
 }
 
-const int net_input_w = 128;
-const int net_input_h = 72;
+const int net_input_w = OUT_WIDTH;
+const int net_input_h = OUT_HEIGHT;
 const int net_grid_w = 16;
 const int net_grid_h = 9;
 
@@ -471,6 +650,9 @@ struct Obj
 	int y;
 	float confidence;
 };
+
+static int tf_mode = 0;
+static bool is_dpy = false;
 
 static std::vector<Obj> getTFObjects(const tensorflow::Tensor& objectness_out_tensor, float threshold = 0.5f)
 {
@@ -513,44 +695,81 @@ static std::vector<Obj> getTFObjects(const tensorflow::Tensor& objectness_out_te
 	return objs;
 }
 
+tensorflow::Tensor is_training_tensor(tensorflow::DT_BOOL, tensorflow::TensorShape());
+
+tensorflow::Tensor cuda_inp_tensor;
+
+std::string tensor_name;
+
+tensorflow::Tensor tmp_inp_tensor;
+
 static void feedTFFrame(const cv::Mat& bgra_frame)
 {
-	void* vdpau_ptr = TFVDPAUmap();
+	tensorflow::Tensor* inp_tensor = NULL;
 
-	boost::shared_ptr<VDPAUAllocator> allocator(new VDPAUAllocator(vdpau_ptr));
-	tensorflow::Tensor inp_tensor(allocator.get(), tensorflow::DataType::DT_UINT8, tensorflow::TensorShape({1, net_input_h, net_input_w, 4}));
+	if (tf_mode == 2) {
+		CUDA_MEMCPY2D cpyinfo;
 
-	/*tensorflow::Tensor inp_tensor(tensorflow::DT_UINT8,
-		tensorflow::TensorShape({1, net_input_h, net_input_w, 4}));
-	unsigned char* inp_data = inp_tensor.flat<unsigned char>().data();
-	memcpy(inp_data, bgra_frame.data, net_input_w * net_input_h * 4);*/
+		cpyinfo.srcMemoryType = CU_MEMORYTYPE_ARRAY;
+		cpyinfo.srcHost = 0;
+		cpyinfo.srcArray = array;
+		cpyinfo.srcXInBytes = 0;
+		cpyinfo.srcY = 0;
+		cpyinfo.srcPitch = OUT_WIDTH * 4;
+		cpyinfo.dstMemoryType = CU_MEMORYTYPE_DEVICE;
+		cpyinfo.dstDevice = vdpau_cuda_data;
+		cpyinfo.dstXInBytes = 0;
+		cpyinfo.dstY = 0;
+		cpyinfo.dstPitch = OUT_WIDTH * 4;
+		cpyinfo.WidthInBytes = OUT_WIDTH * 4;
+		cpyinfo.Height = OUT_HEIGHT;
+
+		CUresult res = cuMemcpy2DAsync(&cpyinfo, tf_cuda_stream);
+		assert(res == CUDA_SUCCESS);
+
+		inp_tensor = &cuda_inp_tensor;
+	} else {
+		tmp_inp_tensor = tensorflow::Tensor(tensorflow::DT_UINT8,
+			tensorflow::TensorShape({1, net_input_h, net_input_w, 4}));
+		unsigned char* inp_data = tmp_inp_tensor.flat<unsigned char>().data();
+		memcpy(inp_data, bgra_frame.data, net_input_w * net_input_h * 4);
+		inp_tensor = &tmp_inp_tensor;
+	}
 
 	std::vector<tensorflow::Tensor> outputs;
 
-	tensorflow::Tensor is_training_tensor(tensorflow::DT_BOOL, tensorflow::TensorShape());
 	is_training_tensor.scalar<bool>()() = false;
 
 	auto status = tf_session->Run(
-		{{"Placeholder", is_training_tensor}, {"input*0", inp_tensor}},
-		{"net_objectness_out", "net_convlast_copy"}, {}, &outputs);
+		{{"Placeholder", is_training_tensor}, {tensor_name, *inp_tensor}},
+		{"net_objectness_out"}, {}, &outputs);
 	assert(status.ok());
 
-	auto objs = getTFObjects(outputs[0], 0.5f);
+	if (is_dpy) {
+		auto objs = getTFObjects(outputs[0], 0.5f);
 
-	for (auto it = objs.begin(); it != objs.end(); ++it) {
-		printf("x:%d, y:%d, conf:%f\n", it->x, it->y, it->confidence);
+		for (auto it = objs.begin(); it != objs.end(); ++it) {
+			printf("x:%d, y:%d, conf:%f\n", it->x, it->y, it->confidence);
+		}
 	}
-
-	TFVDPAUunmap();
 }
 
 int main(int argc, char* argv[])
 {
+	if (argc <= 3) {
+		printf("vdapp [file] [cuda] [dpy]\n");
+		return 0;
+	}
+
+	CompressedVideoStream compressed_vid_stream(argv[1]);
+	tf_mode = atoi(argv[2]);
+	is_dpy = atoi(argv[3]);
+
 	XEvent event;
 
 	vdp_chroma_type = VDP_CHROMA_TYPE_420;
-	vid_width = MY_VID_WIDTH;
-	vid_height = MY_VID_HEIGHT;
+	vid_width = compressed_vid_stream.get_width();
+	vid_height = compressed_vid_stream.get_height();
 	colorspace = 1;
 	VdpVideoMixerPictureStructure field = VDP_VIDEO_MIXER_PICTURE_STRUCTURE_FRAME;
 	procamp.struct_version = VDP_PROCAMP_VERSION;
@@ -559,6 +778,8 @@ int main(int argc, char* argv[])
 	procamp.saturation = 1.0;
 	procamp.hue = 0.0;
 	int status;
+
+	printf("width = %d, height = %d\n", vid_width, vid_height);
 
 	initX();
 
@@ -589,39 +810,54 @@ int main(int argc, char* argv[])
 		printf("Error in initializing TF\n");
 	}
 
-	status = putBits();
-	if (status == -1)
-		printf("Error in Putting data on VdpVideoSurface\n");
+	createDecoder();
 
-	vdp_video_mixer_render(video_mixer, VDP_INVALID_HANDLE, 0,
-										field, 0, (VdpVideoSurface*)VDP_INVALID_HANDLE,
-										video_surface,
-										0, (VdpVideoSurface*)VDP_INVALID_HANDLE,
-										NULL,
-										output_surface,
-										NULL, NULL, 0, NULL);
+	struct timeval tv1;
+	gettimeofday(&tv1, NULL);
 
-	cv::Mat frame = getBits();
-	feedTFFrame(frame);
-	feedTFFrame(frame);
-	feedTFFrame(frame);
+	int num_frames = 0;
 
-	int i = 1;
-	while (i) {
-	   vdp_presentation_queue_display(vdp_queue,
-											  output_surface,
-											  0, 0,
-											  0);
-	   i--;
+	cv::Mat tmp_mat;
+
+	if (tf_mode == 2) {
+		TFVDPAUmap();
+
+		boost::shared_ptr<VDPAUAllocator> allocator(new VDPAUAllocator((void*)vdpau_cuda_data));
+		cuda_inp_tensor = tensorflow::Tensor(allocator.get(), tensorflow::DataType::DT_UINT8, tensorflow::TensorShape({1, net_input_h, net_input_w, 4}));
+		tensor_name = "input*0";
+	} else {
+		tensor_name = "input";
 	}
 
-	while (true) {
-	   XNextEvent(dpy, &event);
-	   if (event.type == KeyPress) {
-		   cleanupX();
-		   break;
-	   }
+	while (compressed_vid_stream.get_next_frame()) {
+		if (is_dpy) {
+			vdp_presentation_queue_display(vdp_queue,
+				output_surface,
+				0, 0,
+				0);
+		}
+		if (tf_mode == 2) {
+			feedTFFrame(tmp_mat);
+		} else if (tf_mode == 1) {
+			feedTFFrame(getBits());
+		}
+		++num_frames;
 	}
+
+	struct timeval tv2, tv_res;
+	gettimeofday(&tv2, NULL);
+
+	timersub(&tv2, &tv1, &tv_res);
+
+	double tm = (double)tv_res.tv_sec + (double)tv_res.tv_usec / 1000000.0;
+
+	printf("DONE %f fps!\n", (float)(num_frames / tm));
+
+	if (tf_mode == 2) {
+		TFVDPAUunmap();
+	}
+
+	cleanupX();
 
 	return 0;
 }
